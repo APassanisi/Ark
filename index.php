a6048e978ea6357421e9b6e1ee957f3d9aa35b8d
TheSporkBaron <passanisi.andrew@gmail.com>
2015-04-11 04:23:57 -0700

    added buttons to navbar

--- index.php
+++ index.php
@@ -4,19 +4,6 @@
 <?php require_once('includes/database.php'); ?>
 
 
-  <div class="jumbotron">
-    <div class="container">
-      <h1>Hello, world!</h1>
-
-      <p>This is a template for a simple marketing or informational website. It
-      includes a large callout called a jumbotron and three supporting pieces
-      of content. Use it as a starting point to create something more
-      unique.</p>
-
-      <p><a class="btn btn-primary btn-lg" href="#">Learn more &raquo;</a></p>
-    </div>
-  </div>
-
   <div class="container">
 
     <div class="row">
