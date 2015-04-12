8f6ba2d117b84b85620612a06188c119ebfc9db2
TheSporkBaron <passanisi.andrew@gmail.com>
2015-04-12 04:50:39 -0700

    Got hompage in working order (no style yet)

--- includes/header.php
+++ includes/header.php
@@ -50,57 +50,52 @@ if(isset($_COOKIE['username'])) {
 ?>
 
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta content="IE=edge" http-equiv="X-UA-Compatible">
-  <meta content="width=device-width, initial-scale=1" name="viewport">
-  <meta content="" name="description">
-  <meta content="" name="author">
-  <link href="favicon.ico" rel="icon">
-
-  <title>Ark3 Project</title>
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta content="" name="description">
+    <meta content="" name="author">
+    <link href="favicon.ico" rel="icon">
+    <title>Ark3 Project</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/jquery.powertip.min.css" rel="stylesheet">
-  <!-- CSS for main site -->
-  <link href="css/main.css" rel="stylesheet">
-  <!-- CSS for character creator -->
-  <link href="css/charactercreator.css" rel="stylesheet">
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/charactercreator.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 
 <body>
-  <nav class="navbar navbar-default">
+<nav class="navbar navbar-default">
     <div class="container">
-      <div class="navbar-header">
-        <button class="navbar-toggle collapsed" data-target="#navbar"
-        data-toggle="collapse" type="button"><span class="sr-only">Toggle
-        navigation</span> <span class="icon-bar"></span> <span class=
-        "icon-bar"></span> <span class="icon-bar"></span></button>
-      </div>
+        <div class="navbar-header">
+            <button class="navbar-toggle collapsed" data-target="#navbar" data-toggle="collapse" type="button">
+                <span class="sr-only">Toggle navigation</span> 
+                <span class="icon-bar"></span> <span class="icon-bar"></span> 
+                <span class="icon-bar"></span>
+            </button>
+        </div>
 
-      <div class="navbar-collapse collapse" id="navbar">
+        <div class="navbar-collapse collapse" id="navbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="About.php">About</a>
+                </li>
 
-        <ul class="nav navbar-nav navbar-right">
-        <li>
-            <a href="About.php">About</a>
-          </li>
+                <li>
+                    <a href="Guide.php">Guide</a>
+                </li>
 
-          <li>
-            <a href="Guide.php">Guide</a>
-          </li>
-
-          <li>
-            <a href="index.php">Home</a>
-          </li>
-        <?php echo $log_display; ?>
-        </ul>
-      </div><!--/.nav-collapse -->
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                    <?php echo $log_display; ?>
+                </ul>
+        </div><!--/.nav-collapse -->
     </div>
-  </nav>
+</nav>
--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <?php include 'includes/header.php'; ?>
 
 <div class="container">
-    <div class="row">
-        <section class="splash alert alert-info">
-            <div class="tree">
-                <img src="img/arktree.jpg">
-            </div>
 
-            <div class="logo">
-                <img src="img/arklogo.jpg">
-            </div>
+    <div class="row">
+
+        <section class="splash hidden-xs alert alert-info col-xs-12">
+
+            <img class="tree" src="img/arktree.jpg">
+
+            <img class="logo pull-right" src="img/arklogo.jpg">
+
         </section>
+
+        <section class="splash-mobile visible-xs alert alert-info col-xs-12">
+
+        </section>
+
     </div>
+
+    <div class="row">
+
+        <div class="about alert alert-info col-xs-12">
+
+        </div>
+
+    </div>
+
 </div>
 
 <?php include 'includes/footer.php'; ?>
