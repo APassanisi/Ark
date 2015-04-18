1e9c8f7e12becbc4a84f419f04dff3418c443eb9
TheSporkBaron <passanisi.andrew@gmail.com>
2015-04-18 01:27:13 -0700

    set colors and css styling - still needs work

---
+++ newArk/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+    <nav class="navbar navbar-fixed-top">
+
+        <div class="container">
+
+            <div class="navbar-header">
+
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+
+                    <span class="glyphicon glyphicon-chevron-down"></span>
+
+                </button>
+
+                <a class="navbar-brand" href="index.php">The Ark</a>
+
+            </div>
+
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+
+                    <li>
+                        <a href="#gameguide">Game Guide</a>
+                    </li>
+
+                    <li>
+                        <a href="#charactercreator">Character Creator</a>
+                    </li>
+
+                    <li>
+                        <a href="#account">Account</a>
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </div>  <!-- /.navbar container -->
+
+    </nav>
+
+    <section id="home">
+
+    </section>
+
+    <section id="about">
+
+    </section>
+
+    <section id="gameguide">
+
+    </section>
+
+    <section id="charactercreator">
+
+    </section>
+
+    <section id="account">
+
+    </section>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+</html>
