1642b1d52a50898708a05f84d6889b2c9c4bdc9a
Andrew Passanisi <passanisi.andrew@gmail.com>
2015-04-24 01:40:36 -0700

    Filled out more template stuff for gameguide.html

--- restart/app/scripts/main.js
+++ restart/app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+	$('.races').click(function () {
+		$('.guide-topics li').removeClass('active');
+		$(this).addClass('active');
+		$('.guide-page').hide()
+			.load('/gameguidepages/races.html')
+			.fadeIn();
+	});
+
+	$('.professions').click(function () {
+		$('.guide-topics li').removeClass('active');
+		$(this).addClass('active');
+		$('.guide-page').hide()
+			.load('/gameguidepages/professions.html')
+			.fadeIn();
+	});
+
+	$('.game-mechanics').click(function () {
+		$('.guide-topics li').removeClass('active');
+		$(this).addClass('active');
+		$('.guide-page').hide()
+			.load('/gameguidepages/gamemechanics.html')
+			.fadeIn();
+	});
+});
