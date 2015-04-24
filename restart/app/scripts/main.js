bacacad2c3feec2dfb26ec553224c08bb8555228
Andrew Passanisi <passanisi.andrew@gmail.com>
2015-04-24 04:29:13 -0700

    Refactored ajax calls

--- restart/app/scripts/main.js
+++ restart/app/scripts/main.js
@@ -1,6 +1,42 @@
 $(document).ready(function () {
     'use strict';
 
+    //Initializer - Fills app-container with homepage content.
+    var init = function init () {
+        $('.home').addClass('active');
+        $('.app-container').load('includes/home.html')
+            .fadeIn();
+    };
+
+    //Ajax loaders for navbar pages.
+    $('.home').click(function (event) {
+        event.preventDefault();
+        $('.nav li').removeClass('active');
+        $(this).addClass('active');
+        $('.app-container').hide()
+            .load('includes/home.html')
+            .fadeIn();
+    });
+
+    $('.gameguide').click(function (event) {
+        event.preventDefault();
+        $('.nav li').removeClass('active');
+        $(this).addClass('active');
+        $('.app-container').hide()
+            .load('includes/gameguide.html')
+            .fadeIn();
+    });
+
+    $('.charactercreator').click(function (event) {
+        event.preventDefault();
+        $('.nav li').removeClass('active');
+        $(this).addClass('active');
+        $('.app-container').hide()
+            .load('includes/charactercreator.html')
+            .fadeIn();
+    });
+
+    //Game guide ajax calls for content.
     $('.races').click(function () {
         $('.guide-topics li').removeClass('active');
         $(this).addClass('active');
@@ -24,4 +60,6 @@ $(document).ready(function () {
             .load('/gameguidepages/gamemechanics.html')
             .fadeIn();
     });
+
+    init();
 });
