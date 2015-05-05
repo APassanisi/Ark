5d028426d0eb64cab18c6f2a92600b06aaa2a2f8
Andrew Passanisi <passanisi.andrew@gmail.com>
2015-05-05 05:47:22 -0700

    Added descriptions for race/class in JS files

--- app/scripts/Model.js
+++ app/scripts/Model.js
@@ -54,6 +54,28 @@ ARK3.Model = (function ($) {
     // var ironBreastplate = new Armor('Iron breastplate', 4, -3);
     // var ironBoots = new Armor('Iron boots', 2, -2);
 
+    var humanDescripton = 'Humans are probably the most numerous and wide spread race. Perhaps well aided by their flexibility and self sufficiency they have expanded at a rate not matched by any other race, and place considerable value on the merits of exploration and discovery.';
+
+    var robotDescripton = 'Robots are constructs of metal and gears. Their origins as a race are mostly unknown, even to them. They constantly repair themselves, and as long as they can acquire the necessary parts, are immune to aging. Robot communities are uncommon, and exist mainly to offer maintanaince. Most of the other organic races feel uncomfortable with allowing them to participate in their own communities.';
+
+    var barbarianDescripton = 'Barbarians are generally characterized as a rough, somewhat primitive people, and while their society has advanced beyond a tribal level, they maintain many aspects of tribal culture even today. They are naturally warlike and have a strong affinity for combat. Barbarians also have a strong sense of loyalty, honor, and justice, and admire these qualities in others. They are generally not a deeply intellectual or philisophical race, and even the religious among them tend to prefer simple, physical pleasures to intensive study or mental challenges.';
+
+    var dwarfDescripton = 'The dwarves are an old, hardy race, older than the humans and possibly even older than the elves. They live within the earth, in a harmony that often boggles more surface-bound races - even a hill dwarf can sniff a handful of dirt and tell whether it would be good for planting crops, for example. Their mountain cousins, meanwhile, have such a keen sense of the stone around them that they can tell worked stone from natural hollows, and even how far down they are.';
+
+    var elfDescripton = 'The Elves’ very slenderness tends to make them seem taller than Humans, though their actual size varies depending on where they call home. The most common variety hail from forests, but ones living in mountains or seas are not unknown. Wherever they come from, they all share an affinity for nature and a sense of duty toward the animals and things of their native environment.';
+
+
+    var warriorDescripton = 'Some take up arms for glory, wealth, or revenge. Others do battle to prove themselves, to protect others, or because they know nothing else. Still others learn the ways of weaponcraft to hone their bodies in battle and prove their mettle in the forge of war. Lords of the battlefield, warriors are a disparate lot, training with many weapons or just one, perfecting the uses of armor, learning the fighting techniques of exotic masters, and studying the art of combat, all to shape themselves into living weapons. Far more than mere thugs, these skilled warriors reveal the true deadliness of their weapons, turning hunks of metal into arms capable of taming kingdoms, slaughtering monsters, and rousing the hearts of armies. Soldiers, knights, hunters, and artists of war, warriors are unparalleled champions, and woe to those who dare stand against them.';
+
+    var wizardDescripton = 'Beyond the veil of the mundane hide the secrets of absolute power. The works of beings beyond mortals, the legends of realms where gods and spirits tread, the lore of creations both wondrous and terrible—such mysteries call to those with the ambition and the intellect to rise above the common folk to grasp true might. Such is the path of the wizard. These shrewd magic-users seek, collect, and covet esoteric knowledge, drawing on cultic arts to work wonders beyond the abilities of mere mortals. While some might choose a particular field of magical study and become masters of such powers, others embrace versatility, reveling in the unbounded wonders of all magic. In either case, wizards prove a cunning and potent lot, capable of smiting their foes, empowering their allies, and shaping the world to their every desire.';
+
+    var thiefDescripton = 'Life is an endless adventure for those who live by their wits. Ever just one step ahead of danger, thieves bank on their cunning, skill, and charm to bend fate to their favor. Never knowing what to expect, they prepare for everything, becoming masters of a wide variety of skills, training themselves to be adept manipulators, agile acrobats, shadowy stalkers, or masters of any of dozens of other professions or talents. Thieves and gamblers, fast talkers and diplomats, bandits and bounty hunters, and explorers and investigators all might be considered thieves, as well as countless other professions that rely upon wits, prowess, or luck. Although many thieves favor cities and the innumerable opportunities of civilization, some embrace lives on the road, journeying far, meeting exotic people, and facing fantastic danger in pursuit of equally fantastic riches. In the end, any who desire to shape their fates and live life on their own terms might come to be called thieves.';
+
+    var explorerDescripton = 'Explorers take pride in discovering the unknown and finding places and things no one else has seen. Often found in the wilds, explorers make excellent hunters.Knowledgeable, patient, and skilled hunters, explorers hound man, beast, and monster alike, gaining insight into the way of the predator, skill in varied environments, and ever more lethal martial prowess. While some track man-eating creatures to protect the frontier, others pursue more cunning game—even fugitives among their own people.';
+
+    var detectiveDescripton = 'Whether on the trail of a fugitive, a long-lost treasure trove, or a criminal mastermind, detectives are motivated by an intense curiosity about the world and use knowledge of it as a weapon. Mixing gumption and learnedness into a personal alchemy of daring, detectives are full of surprises. Observing the world around them, they gain valuable knowledge about the situation theyre in, process that information using inspiration and deduction, and cut to the quick of the matter in unexpected ways. Detectives are always evaluating situations they encounter, sizing up potential foes, and looking out for secret dangers, all while using their vast knowledge and powers of perception to find solutions to the most perplexing problems.';
+
+
     var characterOptions = {
 
         genderArray: [
@@ -69,7 +91,7 @@ ARK3.Model = (function ($) {
             wit: 11,
             per: 11,
             lck: 11,
-            description: 'You are a Human.'
+            description: '<h3>You are a Human.</h3>' + humanDescripton
         }, {
             race: 'robot',
             str: 11,
@@ -78,7 +100,7 @@ ARK3.Model = (function ($) {
             wit: 16,
             per: 5,
             lck: 11,
-            description: 'You are a Robot.'
+            description: '<h3>You are a Robot.</h3>' + robotDescripton
         }, {
             race: 'barbarian',
             str: 11,
@@ -87,7 +109,7 @@ ARK3.Model = (function ($) {
             wit: 11,
             per: 5,
             lck: 11,
-            description: 'You are a Barbarian.'
+            description: '<h3>You are a Barbarian.</h3>' + barbarianDescripton
         }, {
             race: 'dwarf',
             str: 13,
@@ -96,7 +118,7 @@ ARK3.Model = (function ($) {
             wit: 8,
             per: 8,
             lck: 11,
-            description: 'You are a Dwarf.'
+            description: '<h3>You are a Dwarf.</h3>' + dwarfDescripton
         }, {
             race: 'elf',
             str: 8,
@@ -105,7 +127,7 @@ ARK3.Model = (function ($) {
             wit: 13,
             per: 11,
             lck: 11,
-            description: 'You are an Elf.'
+            description: '<h3>You are a Elf.</h3>' + elfDescripton
         }],
 
         weaponsArray: [
@@ -128,7 +150,7 @@ ARK3.Model = (function ($) {
             chest: chainShirt,
             feet: chainBoots,
             weapon: longsword,
-            description: 'You are a warrior.'
+            description: '<h3>You are a warrior.</h3>' + warriorDescripton
         }, {
             profession: 'wizard',
             strBonus: 0,
@@ -141,7 +163,7 @@ ARK3.Model = (function ($) {
             chest: clothRobe,
             feet: clothSlippers,
             weapon: fire,
-            description: 'You are a wizard.'
+            description: '<h3>You are a wizard.</h3>' + wizardDescripton
         }, {
             profession: 'thief',
             strBonus: 0,
@@ -154,7 +176,7 @@ ARK3.Model = (function ($) {
             chest: leatherJerkin,
             feet: clothSlippers,
             weapon: dagger,
-            description: 'You are a thief.'
+            description: '<h3>You are a thief.</h3>' + thiefDescripton
         }, {
             profession: 'explorer',
             strBonus: 2,
@@ -167,7 +189,7 @@ ARK3.Model = (function ($) {
             chest: leatherJerkin,
             feet: leatherBoots,
             weapon: axe,
-            description: 'You are a explorer.'
+            description: '<h3>You are a explorer.</h3>' + explorerDescripton
         }, {
             profession: 'detective',
             strBonus: 0,
@@ -180,7 +202,7 @@ ARK3.Model = (function ($) {
             chest: leatherJerkin,
             feet: leatherBoots,
             weapon: pistols,
-            description: 'You are a detective.'
+            description: '<h3>You are a detective.</h3>' + detectiveDescripton
         }],
 
         hairColorArray: [
